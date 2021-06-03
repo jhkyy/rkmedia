15f729489bfcd5f3cf968b193a979fcfdb9d3879
ctf <ctf@rock-chips.com>
2021-06-03 17:41:49 +0800

    c_api: fix jpeg SetBitMap
    Signed-off-by: ctf <ctf@rock-chips.com>
    Change-Id: Ibf9bba4b9f5e18c4d4592df1bcfc455a5fa1d7f1

--- src/c_api/rkmedia_api.cc
+++ src/c_api/rkmedia_api.cc
@@ -4782,8 +4782,15 @@ RK_S32 RK_MPI_VENC_RGN_SetBitMap(VENC_CHN VeChn,
   rkmedia_osd_rgn.inverse = pstRgnInfo->u8Inverse;
   rkmedia_osd_rgn.enable = pstRgnInfo->u8Enable;
   rkmedia_osd_rgn.region_type = region_type;
-  ret = easymedia::video_encoder_set_osd_region(g_venc_chns[VeChn].rkmedia_flow,
-                                                &rkmedia_osd_rgn, u8PlaneCnt);
+
+  if (g_venc_chns[VeChn].venc_attr.bFullFunc)
+    ret = easymedia::video_encoder_set_osd_region(
+        g_venc_chns[VeChn].rkmedia_flow_list.back(), &rkmedia_osd_rgn,
+        u8PlaneCnt);
+  else
+    ret = easymedia::video_encoder_set_osd_region(
+        g_venc_chns[VeChn].rkmedia_flow, &rkmedia_osd_rgn, u8PlaneCnt);
+
   if (ret)
     ret = -RK_ERR_VENC_NOT_PERM;
 
